4e0fb9da079df095c51ae50d158e40c32ef82d44
Nelliosis <jcapawing@gmail.com>
2024-03-26 03:12:46 +0800

    add login route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 
 import { ping } from './src/controllers/ping.js'
+import { login } from './src/controllers/login.js'
 
 const app = express()
 const port = 3000
@@ -11,5 +12,6 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/ping', ping)
+app.get('/login', login)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
