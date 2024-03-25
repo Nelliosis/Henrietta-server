381b11fb7a65ec73fdd1b240c3089bb767df2003
Nelliosis <jcapawing@gmail.com>
2024-03-26 05:23:10 +0800

    init knexfile config

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } = process.env
+
+export default {
+
+  development: {
+    client: 'postgresql',
+
+    connection: {
+      database: DATABASE_NAME,
+      user: DATABASE_USER,
+      password: DATABASE_PASSWORD,
+      host: 'henrietta_db',
+      port: 5432
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
+
